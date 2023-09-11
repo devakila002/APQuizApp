5b961cf2909697b98768636ba01a57bd3e941734
devakila <dev.akila001@gmail.com>
2023-09-11 20:21:12 +0530

    Created a QuizQustion.cs

---
+++ APQiuzAPP/Models/QuizQuestion.cs
@@ -0,0 +1,18 @@
+namespace APQuizApp.Models
+{
+    public class QuizQuestion
+    {
+        public string QuestionText { get; set; } = string.Empty;
+        public string Option01Text { get; set; } = string.Empty;
+        public string Option02Text { get; set; } = string.Empty;
+        public string Option03Text { get; set; } = string.Empty;
+        public string Option04Text { get; set; } = string.Empty;
+        public string CorrectAnswerText { get; set; } = string.Empty;
+
+    }
+
+}
+
+
+
+
